55b3657058b022e98df6e098872724bff05b1d02
shauryagoyal1943-ops <shauryagoyal1943@gmail.com>
2026-08-06 19:19:22 +0530

    Add files via upload

---
+++ Area.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int length=12;
+    int breadth=23;
+    int area=length*breadth;
+    printf("The rectangle with length %d and breadth %d has area=%d",length,breadth,area);
+    return 0;
+}

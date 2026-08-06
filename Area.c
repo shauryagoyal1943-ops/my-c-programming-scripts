3100b3956a8d47c3cc81107f8c278591c0aa3ee7
shauryagoyal1943-ops <shauryagoyal1943@gmail.com>
2026-08-06 19:21:42 +0530

    Delete Area.c

--- Area.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int length=12;
-    int breadth=23;
-    int area=length*breadth;
-    printf("The rectangle with length %d and breadth %d has area=%d",length,breadth,area);
-    return 0;
-}
